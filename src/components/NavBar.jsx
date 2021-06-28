12ebbc80352b8c08781a629f2f753e6b7313ed3d
crystal <noelani.villanueva@gmail.com>
2021-06-28 15:08:00 -0700

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 // import { NavLink } from "react-router-dom";
 
 const NavBar = (props) => {
-  console.log(props);
   return (
     <div className="navBarSpace">
       <button className="logout-button" onClick={props.handleLogout}>
